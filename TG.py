68e9fc7e6a5817dce0a623e2ba82cb7a2dd0e862
princesshavy <havilah757@gmail.com>
2024-09-14 16:29:19 +0100

    telegram bot

--- TG.py
+++ TG.py
@@ -1,7 +1,7 @@
 import requests
 
 # Your bot's API token
-TOKEN = 'YOUR_API_TOKEN_HERE'
+TOKEN = '7321066472:AAEcgCI50MUDyl1Khmgc1qVAl-oQN0qAPfc'
 URL = f'https://api.telegram.org/bot{TOKEN}/'
 
 def get_updates(offset=None):
@@ -24,12 +24,15 @@ def main():
         for update in updates['result']:
             message_text = update['message']['text']
             chat_id = update['message']['chat']['id']
-
-            if message_text == '/start':
-                send_message(chat_id, 'Welcome to the bot!')
+           
+            # Using if-elif-else statement
+            if message_text == 'hi':
+                send_message(chat_id, 'Hello! Welcome to my world')
+            elif message_text == 'What is your name':
+                send_message(chat_id, 'My name is havybot, and I was created by 2 crazy developers Havilah and Quantum Ben')
             else:
-                send_message(chat_id, f'You said: {message_text}')
-
+                send_message(chat_id, f'Hey unknown, you said: {message_text}')
+               
             offset = update['update_id'] + 1
 
 if __name__ == '__main__':
